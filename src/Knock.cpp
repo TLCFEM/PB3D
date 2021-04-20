898df9b59b18e8b61f364771f5c5ca22642a7000
tlc@workhorse <tlcfem@gmail.com>
2021-04-20 16:05:45 +1200

    INITIAL COMMIT

---
+++ src/Knock.cpp
@@ -0,0 +1,88 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2021 Theodore Chang, Minghao Li
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+////////////////////////////////////////////////////////////////////////////////
+
+#include "ModelBuilder.h"
+
+#include <QApplication>
+#include <QScreen>
+#include <QSettings>
+#include <QStyleFactory>
+#include <QSurfaceFormat>
+
+bool PB3D_DARK = false;
+
+int main(int argc, char* argv[]) {
+	QApplication app(argc, argv);
+    QApplication::setApplicationName("PB3D");
+    QApplication::setApplicationDisplayName("PB3D");
+	QApplication::setOrganizationName("University of Canterbury");
+	QApplication::setWindowIcon(QIcon(":/../res/UC.ico"));
+
+	auto font = QApplication::font();
+	const auto rec = QGuiApplication::primaryScreen()->availableGeometry();
+	if(std::max(rec.height(), rec.width()) > 2000) font.setPointSize(12);
+	else font.setPointSize(9);
+	QApplication::setFont(font);
+
+	qApp->setStyle(QStyleFactory::create("fusion"));
+
+#ifdef Q_OS_WIN
+	const QSettings settings(R"(HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize)", QSettings::NativeFormat);
+	if(settings.value("AppsUseLightTheme") == 0) {
+		QPalette darkPalette;
+		const auto darkColor = QColor(45, 45, 45);
+		const auto disabledColor = QColor(209, 17, 65);
+		darkPalette.setColor(QPalette::Window, darkColor);
+		darkPalette.setColor(QPalette::WindowText, Qt::white);
+		darkPalette.setColor(QPalette::Base, QColor(18, 18, 18));
+		darkPalette.setColor(QPalette::AlternateBase, darkColor);
+		darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+		darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+		darkPalette.setColor(QPalette::Text, Qt::white);
+		darkPalette.setColor(QPalette::Disabled, QPalette::Text, disabledColor);
+		darkPalette.setColor(QPalette::Button, darkColor);
+		darkPalette.setColor(QPalette::ButtonText, Qt::white);
+		darkPalette.setColor(QPalette::Disabled, QPalette::ButtonText, disabledColor);
+		darkPalette.setColor(QPalette::BrightText, Qt::red);
+		darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+
+		darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+		darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+		darkPalette.setColor(QPalette::Disabled, QPalette::HighlightedText, disabledColor);
+
+		qApp->setPalette(darkPalette);
+
+		qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
+
+        PB3D_DARK = true;
+	}
+#endif
+
+	ModelBuilder win;
+    win.setWindowTitle("PB3D");
+
+	QSurfaceFormat format;
+	format.setDepthBufferSize(24);
+	format.setStencilBufferSize(8);
+	format.setVersion(2, 0);
+	format.setProfile(QSurfaceFormat::CompatibilityProfile);
+	QSurfaceFormat::setDefaultFormat(format);
+
+	win.show();
+
+	return QApplication::exec();
+}
---
+++ src/ModelBuilder.h
@@ -0,0 +1,117 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2021 Theodore Chang, Minghao Li
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef MODELBUILDER_H
+#define MODELBUILDER_H
+
+#include <Database.h>
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+
+namespace Ui {
+	class ModelBuilder;
+}
+
+QT_END_NAMESPACE
+
+class ModelBuilder final : public QMainWindow {
+Q_OBJECT
+public:
+	explicit ModelBuilder(QWidget* = nullptr);
+	~ModelBuilder() override;
+
+public slots:
+	void highlightNode(const QString&, int);
+	void highlightNodeA(QString);
+	void highlightNodeB(QString);
+	void highlightNodeC(QString);
+	void highlightNodeD(QString);
+	void highlightNodeE();
+
+	void highlightElement(const QString&, int);
+	void highlightElementA(QString);
+
+	void showAbout();
+	void openFile();
+	void writeOutput();
+	void saveScreenshot();
+
+private slots:
+	void on_box_element_currentTextChanged(const QString&);
+	void on_box_element_type_currentTextChanged(const QString&);
+	void on_box_frame_section_textHighlighted(const QString&);
+	void on_box_load_type_currentTextChanged(const QString&) const;
+	void on_box_modify_type_currentIndexChanged(int) const;
+	void on_box_section_2_textHighlighted(const QString&);
+	void on_box_section_textHighlighted(const QString&);
+	void on_box_unit_currentIndexChanged(int);
+	void on_box_wall_section_textHighlighted(const QString&);
+	void on_button_add_bc_clicked();
+	void on_button_add_element_clicked();
+	void on_button_add_frame_section_clicked();
+	void on_button_add_load_clicked();
+	void on_button_add_node_clicked();
+	void on_button_add_wall_section_clicked();
+	void on_button_change_section_clicked();
+	void on_button_clear_bc_clicked();
+	void on_button_clear_load_clicked();
+	void on_button_modify_node_clicked();
+	void on_button_remove_all_element_clicked();
+	void on_button_remove_element_clicked();
+	void on_button_remove_frame_section_clicked();
+	void on_button_remove_wall_section_clicked();
+	void on_button_split_element_clicked();
+	void on_input_absf_textChanged(const QString&);
+	void on_input_absu_textChanged(const QString&);
+	void on_input_absx_textChanged(const QString&);
+	void on_input_damping_textChanged(const QString&);
+	void on_input_frame_section_tag_textChanged(const QString&) const;
+    void on_input_period_textChanged(const QString&);
+	void on_input_qfx_textChanged(const QString&);
+	void on_input_qfy_textChanged(const QString&);
+	void on_input_qfz_textChanged(const QString&);
+	void on_input_qwx_textChanged(const QString&);
+	void on_input_qwy_textChanged(const QString&);
+	void on_input_relf_textChanged(const QString&);
+	void on_input_relu_textChanged(const QString&);
+	void on_input_relx_textChanged(const QString&);
+	void on_input_scale_textChanged(const QString&);
+	void on_input_wall_section_tag_textChanged(const QString&) const;
+	void on_reset_model_clicked();
+	void on_box_element_textHighlighted(const QString&);
+	void on_check_accx_clicked(bool);
+	void on_check_accy_clicked(bool);
+	void on_button_select_clicked();
+    void on_button_run_clicked();
+
+private:
+	Ui::ModelBuilder* ui;
+	Database model;
+
+	QVector<int> highlighted_node = QVector<int>(4, 0);
+	QVector<int> highlighted_group = QVector<int>();
+	QVector<int> highlighted_element = QVector<int>(1, 0);
+
+	void updateNodeList() const;
+	void updateFrameSectionList();
+	void updateWallSectionList();
+	void updateElementList() const;
+
+	void updateAnalysisSetting() const;
+};
+#endif // MODELBUILDER_H
